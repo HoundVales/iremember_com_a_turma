13d49eae5bd37585ce1718c2c9d9c1f37f918ed7
joaoc <joaocapitaneo@gmail.com>
2024-08-10 04:06:04 -0300

    adicionado exept para tratar erro na requisição.

--- myapp/views/create_post_view.py
+++ myapp/views/create_post_view.py
@@ -9,24 +9,39 @@ def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            id = 1
+            # Preparar os dados para enviar para a API externa
             data = {
                 'titulo': form.cleaned_data['titulo'],
                 'conteudo': form.cleaned_data['conteudo'],
-                'autor_id':id
+                'autor_id': 1  # Substitua com o ID real do autor se necessário
             }
-            api_response = requests.post('https://api-python-tarde-37668b622097.herokuapp.com/posts', json=data)
-            api_data = api_response.json()
-            if api_response.status_code == 201:
-                success_message2 = 'Post criado com sucesso'
-                url = reverse('info') + f'?api_data={api_data}&info_message={success_message2}'
-                return HttpResponseRedirect(url)
-            else:
-                success_message2 = 'Erro ao criar o post'
-                url = reverse('info') + f'?api_data={api_data}&info_message={success_message2}'
-                return HttpResponseRedirect(url)
+            
+            try:
+                # Enviar dados para a API externa
+                api_response = requests.post('https://api-python-tarde-37668b622097.herokuapp.com/posts', json=data)
+                api_response.raise_for_status()  # Levanta um erro para respostas de erro HTTP
+                
+                # Tentar obter dados da resposta JSON
+                api_data = api_response.json()
+                
+                if api_response.status_code == 201:
+                    success_message2 = 'Post criado com sucesso'
+                else:
+                    success_message2 = 'Erro ao criar o post'
+            
+            except requests.RequestException as e:
+                # Em caso de erro na requisição ou na resposta
+                api_data = {}
+                success_message2 = 'Erro ao criar o post: ' + str(e)
+            
+            # Redirecionar com mensagem de sucesso ou erro
+            url = reverse('info') + f'?api_data={api_data}&info_message={success_message2}'
+            return HttpResponseRedirect(url)
         else:
+            # Se o formulário não for válido, retornar erros no formato JSON
             return JsonResponse({"message": "Erro ao criar o post", "errors": form.errors}, status=400)
     else:
         form = PostForm()
+    
+    # Renderizar o formulário para o GET
     return render(request, 'create_post.html', {'form': form})
