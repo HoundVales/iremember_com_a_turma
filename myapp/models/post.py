e44d1ffd918d7687cff6b83f2fcfc55bdcefa60e
HoundVales <lukkas7425@gmail.com>
2024-08-15 14:22:08 -0300

    ssdsdsds

--- myapp/models/post.py
+++ myapp/models/post.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 class Post(models.Model):
     titulo = models.CharField(max_length=255)
     conteudo = models.TextField()
-    autor_id = models.IntegerField()  # Use ForeignKey se tiver um modelo Autor
+    autor_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    data_publicacao = models.DateTimeField( default=timezone.now)
 
     def __str__(self):
         return self.titulo
+
--- myapp/models/user.py
+++ myapp/models/user.py
@@ -1,9 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class User(models.Model):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('O endereço de e-mail deve ser fornecido')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser):
     nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    cpf = models.CharField(max_length=100, unique=True)
     senha = models.CharField(max_length=100)
-    email = models.EmailField()
-    cpf = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+   
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome', 'cpf']
+
     def __str__(self):
         return self.nome
+
